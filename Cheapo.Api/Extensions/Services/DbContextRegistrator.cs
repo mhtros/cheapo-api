6c6ce58024cc183573b54feb21c73a18d18ef5dc
Panagiotis Mitropanos <mitropanos95@gmail.com>
2022-03-29 14:28:10 +0300

    Register DbContext service

---
+++ Cheapo.Api/Extensions/Services/DbContextRegistrator.cs
@@ -0,0 +1,21 @@
+﻿using Cheapo.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cheapo.Api.Extensions.Services;
+
+public static class DbContextRegistrator
+{
+    /// <summary>
+    ///     Registers the database context as a service in the <see cref="IServiceCollection" />.
+    /// </summary>
+    /// <param name="services"><see cref="IServiceCollection" />.</param>
+    /// <param name="configuration"><see cref="IConfiguration" />.</param>
+    public static void AddDbContextService(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            options.UseNpgsql(connectionString);
+        });
+    }
+}
--- Cheapo.Api/Program.cs
+++ Cheapo.Api/Program.cs
@@ -1,4 +1,7 @@
+using Cheapo.Api.Extensions.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 
 // Add services to the container.
 
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContextService(configuration);
 
 var app = builder.Build();
 
