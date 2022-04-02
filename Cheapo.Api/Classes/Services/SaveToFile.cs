4eb619c3da91e8115b82476f666dc7f578e26d19
Panagiotis Mitropanos <mitropanos95@gmail.com>
2022-04-02 14:53:02 +0300

    SaveToFile service implementation

---
+++ Cheapo.Api/Classes/Services/SaveToFile.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using Cheapo.Api.Interfaces.Services;
+
+namespace Cheapo.Api.Classes.Services;
+
+public class SaveToFile : ISaveToFile
+{
+    /// <summary>
+    ///     Creates a file asynchronously. If file already exist then
+    ///     appends the content at the end of the file.
+    /// </summary>
+    public async Task SaveToFileAsync(string path, string filename, string content)
+    {
+        try
+        {
+            Directory.CreateDirectory(path);
+
+            var filePath = Path.Combine(path, filename);
+            var encodedText = Encoding.Unicode.GetBytes(content);
+
+            const FileMode mode = FileMode.Append;
+            const FileAccess access = FileAccess.Write;
+            const FileShare share = FileShare.None;
+
+            await using var sourceStream = new FileStream(filePath, mode, access, share, 4096, true);
+            await sourceStream.WriteAsync(encodedText.AsMemory(0, encodedText.Length));
+        }
+        catch (IOException ioException)
+        {
+            throw new IOException(ioException.Message);
+        }
+    }
+}
---
+++ Cheapo.Api/Interfaces/Services/ISaveToFile.cs
@@ -0,0 +1,10 @@
+﻿namespace Cheapo.Api.Interfaces.Services;
+
+public interface ISaveToFile
+{
+    /// <summary>
+    ///     Creates a file asynchronously. If file already exist then
+    ///     appends the content at the end of the file.
+    /// </summary>
+    public Task SaveToFileAsync(string path, string filename, string content);
+}
