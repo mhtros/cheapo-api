72a542214de2aded008a897d0438460c55269033
Panagiotis Mitropanos <mitropanos95@gmail.com>
2022-04-02 11:32:40 +0300

    Register (re-configure) Swagger with versioning

---
+++ Cheapo.Api/Extensions/Services/SwaggerRegistrator.cs
@@ -0,0 +1,48 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.OpenApi.Models;
+
+namespace Cheapo.Api.Extensions.Services;
+
+public static class SwaggerRegistrator
+{
+    /// <summary>
+    ///     Registers Swagger in the <see cref="IServiceCollection" />.
+    /// </summary>
+    /// <param name="services"><see cref="IServiceCollection" />.</param>
+    public static void AddSwaggerService(this IServiceCollection services)
+    {
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Version = "ν1",
+                Title = "Cheapo API",
+                Description = "A simple way to  keep track your monthly expenses.",
+                License = new OpenApiLicense
+                {
+                    Name = "MIT License",
+                    Url = new Uri("https://opensource.org/licenses/MIT")
+                }
+            });
+
+            // Setup swagger to display XML comments
+            var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+        });
+
+        // Add swagger versioning
+        services.AddApiVersioning(config =>
+        {
+            config.DefaultApiVersion = new ApiVersion(1, 0);
+            config.AssumeDefaultVersionWhenUnspecified = true;
+            config.ReportApiVersions = true;
+            config.ApiVersionReader = ApiVersionReader.Combine(
+                new HeaderApiVersionReader("x-api-version"),
+                new QueryStringApiVersionReader("api-version")
+            );
+        });
+    }
+}
--- Cheapo.Api/Program.cs
+++ Cheapo.Api/Program.cs
@@ -6,9 +6,10 @@ var configuration = builder.Configuration;
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerService();
 
 builder.Services.AddDbContextService(configuration);
 
