527369cacb5e7a82297075fdf90025bf8714a7ee
Panagiotis Mitropanos <mitropanos95@gmail.com>
2022-04-10 13:49:59 +0300

    Register JWT authentication service

---
+++ Cheapo.Api/Extensions/Services/JwtAuthenticationRegistrator.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using Cheapo.Api.Classes;
+using Cheapo.Api.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Cheapo.Api.Extensions.Services;
+
+public static class JwtAuthenticationRegistrator
+{
+    /// <summary>
+    ///     Adds JWT authentication.
+    /// </summary>
+    /// <param name="services"><see cref="IServiceCollection" />.</param>
+    /// <param name="configuration"><see cref="IConfiguration" />.</param>
+    public static void AddJwtAuthenticationService(this IServiceCollection services, IConfiguration configuration)
+    {
+        var jwtParameters = configuration.GetSection("JwtParameters").Get<JwtParameters>();
+        services.AddScoped<IJwtParameters, JwtParameters>(_ => jwtParameters);
+
+        services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                var key = Encoding.UTF8.GetBytes(jwtParameters.IssuerSigningKey!);
+
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidIssuer = jwtParameters.Issuer,
+                    ValidAudience = jwtParameters.Audience,
+                    RequireExpirationTime = jwtParameters.RequireExpirationTime,
+                    ValidateIssuerSigningKey = jwtParameters.ValidateIssuerSigningKey,
+                    ValidateIssuer = jwtParameters.ValidateIssuer,
+                    ValidateAudience = jwtParameters.ValidateAudience,
+                    ValidateLifetime = jwtParameters.ValidateLifetime
+                };
+            });
+    }
+}
--- Cheapo.Api/Program.cs
+++ Cheapo.Api/Program.cs
@@ -18,6 +18,8 @@ builder.Services.AddIdentityService();
 
 builder.Services.AddDependencyInjectionService();
 
+builder.Services.AddJwtAuthenticationService(configuration);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -30,6 +32,8 @@ if (app.Environment.IsDevelopment())
 app.UseMiddleware<InternalErrorHandler>();
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
