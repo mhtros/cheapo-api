1ad76359be3a70b1ab28d0892c37d1b8f7df4ac2
Panagiotis Mitropanos <mitropanos95@gmail.com>
2022-04-14 12:32:37 +0300

    Add new errors

--- Cheapo.Api/Classes/Errors.cs
+++ Cheapo.Api/Classes/Errors.cs
@@ -3,5 +3,7 @@
 public static class Errors
 {
     public const string ExpiredToken = "EXPIRED_TOKEN";
+    public const string ExpiredRefreshToken = "EXPIRED_REFRESH_TOKEN";
     public const string UserAlreadyExists = "USER_ALREADY_EXISTS";
+    public const string EmailNotSend = "EMAIL_NOT_SEND";
 }
