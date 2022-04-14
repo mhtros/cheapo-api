66d50c9b9ad8a5b0923fae97fd489444278c2ced
Panagiotis Mitropanos <mitropanos95@gmail.com>
2022-04-14 17:14:21 +0300

    ToUnderscore extension method

---
+++ Cheapo.Api/Extensions/StringConverter.cs
@@ -0,0 +1,9 @@
+﻿namespace Cheapo.Api.Extensions;
+
+public static class StringConverter
+{
+    public static string? ToUnderscore(this string? str)
+    {
+        return str?.ToUpper().Replace(' ', '_').Trim('.');
+    }
+}
