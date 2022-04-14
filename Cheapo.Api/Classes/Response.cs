686b7c703ea4d8b36151d45f14648f2da5fa255a
Panagiotis Mitropanos <mitropanos95@gmail.com>
2022-04-14 17:16:03 +0300

    Response class declaration

---
+++ Cheapo.Api/Classes/Response.cs
@@ -0,0 +1,55 @@
+﻿namespace Cheapo.Api.Classes;
+
+public interface IErrorResponse
+{
+    public IEnumerable<string?> Errors { get; set; }
+}
+
+public interface IDataResponse<T>
+{
+    public T? Data { get; set; }
+}
+
+public class ErrorResponse : IErrorResponse
+{
+    public ErrorResponse()
+    {
+    }
+
+    public ErrorResponse(IEnumerable<string?> errors)
+    {
+        Errors = errors;
+    }
+
+    public IEnumerable<string?>? Errors { get; set; }
+}
+
+public class DataResponse<T> : IDataResponse<T>
+{
+    public DataResponse()
+    {
+    }
+
+    public DataResponse(T? data)
+    {
+        Data = data;
+    }
+
+    public T? Data { get; set; }
+}
+
+public class Response<T> : IDataResponse<T>, IErrorResponse
+{
+    public Response()
+    {
+    }
+
+    public Response(T? data, IEnumerable<string?> errors)
+    {
+        Data = data;
+        Errors = errors;
+    }
+
+    public T? Data { get; set; }
+    public IEnumerable<string?>? Errors { get; set; }
+}
