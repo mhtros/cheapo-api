fa4ca622e0d851511c78f103993c6aed796ddf45
mhtros <mitropanos95@gmail.com>
2022-06-06 11:15:40 +0300

    Create BaseRepository, ISaveable

---
+++ Cheapo.Api/Classes/BaseRepository.cs
@@ -0,0 +1,18 @@
+﻿using Cheapo.Api.Data;
+
+namespace Cheapo.Api.Classes;
+
+public abstract class BaseRepository
+{
+    protected readonly ApplicationDbContext Context;
+
+    protected BaseRepository(ApplicationDbContext context)
+    {
+        Context = context;
+    }
+
+    public async Task<bool> SaveAsync()
+    {
+        return await Context.SaveChangesAsync() > 0;
+    }
+}
---
+++ Cheapo.Api/Interfaces/ISaveable.cs
@@ -0,0 +1,6 @@
+﻿namespace Cheapo.Api.Interfaces;
+
+public interface ISaveable
+{
+    public Task<bool> SaveAsync();
+}
