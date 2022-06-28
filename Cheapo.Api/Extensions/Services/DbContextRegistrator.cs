8ead389bfa39fb3bb246b174077d29c0ea6505bb
mhtros <mitropanos95@gmail.com>
2022-06-28 14:09:49 +0300

    Change db context to include Heroku connection url

--- Cheapo.Api/Extensions/Services/DbContextRegistrator.cs
+++ Cheapo.Api/Extensions/Services/DbContextRegistrator.cs
@@ -14,7 +14,33 @@ public static class DbContextRegistrator
     {
         services.AddDbContext<ApplicationDbContext>(options =>
         {
-            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            string connectionString;
+            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            if (env == "Development")
+            {
+                connectionString = configuration.GetConnectionString("DefaultConnection");
+            }
+            else
+            {
+                // Use connection string provided at runtime by Heroku.
+                var connectionUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+                // Parse connection URL to connection string for Npgsql
+                connectionUrl = connectionUrl?.Replace("postgres://", string.Empty);
+                var pgUserPass = connectionUrl?.Split("@")[0];
+                var pgHostPortDb = connectionUrl?.Split("@")[1];
+                var pgHostPort = pgHostPortDb?.Split("/")[0];
+                var pgDb = pgHostPortDb?.Split("/")[1];
+                var pgUser = pgUserPass?.Split(":")[0];
+                var pgPass = pgUserPass?.Split(":")[1];
+                var pgHost = pgHostPort?.Split(":")[0];
+                var pgPort = pgHostPort?.Split(":")[1];
+
+                connectionString =
+                    $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};SSL Mode=Require;TrustServerCertificate=True";
+            }
+
             options.UseNpgsql(connectionString);
         });
     }
